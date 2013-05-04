213121fdd07d5d5a8570b9e1d8de75ec0ab35405
mikeleeorg <mikeleeorg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-04 23:33:53 +0000

    Added support for new type attribute (valid values are "full", "compact", or "list).
    git-svn-id: https://plugins.svn.wordpress.org/edshelf-widget/trunk@708017 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edshelf-widgets.php
+++ edshelf-widgets.php
@@ -3,7 +3,7 @@
 Plugin Name: edshelf Widgets
 Plugin URI: http://edshelf.com
 Description: Adds a shortcode for embedding an edshelf widget on your site.
-Version: 0.1.0
+Version: 0.2
 Author: edshelf
 Author URI: http://edshelf.com
 */
@@ -14,6 +14,7 @@ Author URI: http://edshelf.com
  */
 define( 'EDSHELF_DEFAULT_COLLECTION_ID', 8786 );
 define( 'EDSHELF_DEFAULT_COLLECTION_HEIGHT', 500 );
+define( 'EDSHELF_DEFAULT_COLLECTION_ID', 'full' );
 
 
 /**
@@ -44,19 +45,24 @@ function edshelf_widgets_admin_page() {
 <hr size="1" style="border:0;border-top:1px solid #ccc;">
 <h3>Collections widget</h3>
 <h4>Quick instructions</h4>
-<p>The collection widget shortcode: <code>[edshelf-collection-widget id="NNNN" height="YYY"]</code></p>
-<p>The collection widget template tag: <code>edshelf_collection_widget( $id, $height );</code></p>
+<p>The collection widget shortcode: <code>[edshelf-collection-widget id="NNNN" height="YYY" type="TTTT"]</code></p>
+<p>The collection widget template tag: <code>edshelf_collection_widget( $id, $height, '$type' );</code></p>
 <h4>Step-by-step instructions</h4>
 <p>How to embed this widget:</p>
 <ol>
     <li>Go to the collection on edshelf that you want to embed. As an example, here is the <a href="http://edshelf.com/profile/mikeleeorg/teacher-created-tools" target="_new">Teacher-Created Tools Collection</a>.</li>
-    <li>Look for the Widget module at the bottom of the right column. In that module will be some code that looks like this:<br><code>&lt;div id="edshelf-widget"&gt;&lt;/div&gt;&lt;script src="//edshelf.com/widgets/collection?id=8786&height=500"&gt;&lt;/script&gt;</code></li>
+    <li>Look for the Widget module at the bottom of the right column. In that module will be some code that looks like this:<br><code>&lt;div id="edshelf-widget"&gt;&lt;/div&gt;&lt;script src="//edshelf.com/widgets/collection?id=8786&height=500&type=full"&gt;&lt;/script&gt;</code></li>
     <li>In that code, look for the ID number of the collection. In our example, it is:<br><code>8786</code>.</li>
-    <li>In your WordPress site, go to the blog post or page on which you want to embed this collection widget. Type in the following WordPress shortcode in the text editor:<br><code>[edshelf-collection-widget id="NNNN" height="YYY"]</code></li>
+    <li>In your WordPress site, go to the blog post or page on which you want to embed this collection widget. Type in the following WordPress shortcode in the text editor:<br><code>[edshelf-collection-widget id="NNNN" height="YYY" type="TTTT"]</code></li>
     <li>NNNN represents a collection's ID number, which you just found earlier. Substitute NNNN with the ID number of the collection you want to embed.</li>
     <li>YYY represents the height of the widget in pixels. Substitute YYY with the height you would like to use. You can keep changing this number until you like the height of the widget. For our example, let's use<br><code>800</code>.</li>
-    <li>In our example, the final shortcode is:<br><code>[edshelf-collection-widget id="8786" height="800"]</code></li>
-    <li>Or, if you know PHP and want to use the template tag, it is:<br><code>edshelf_collection_widget( 8786, 800 );</code></li>
+    <li>TTTT represents the type of widget you would like to display. There are three types: "full", "compact", and "list". If you leave this out, the widget will automatically default to full. The differences between each are:<ul>
+        <li><strong>full</strong> - The widget displays all the info that we display on edshelf.com. This is best used if the widget is embedded into a blog post or wide column.</li>
+        <li><strong>compact</strong> - The widget displays only the icons in the collection and hides the tool names and other related info, so it can squeeze into a sidebar or narrow column.</li>
+        <li><strong>list</strong> - The widget displays all of the notes written alongside a tool. This is best used in a blog post or wide column also.</li>
+    </ul></li>
+    <li>In our example, the final shortcode is:<br><code>[edshelf-collection-widget id="8786" height="800" type="full"]</code></li>
+    <li>Or, if you know PHP and want to use the template tag, it is:<br><code>edshelf_collection_widget( 8786, 800, 'full' );</code></li>
     <li>And you're done!</li>
 </ol>
 <?php
@@ -66,33 +72,35 @@ function edshelf_widgets_admin_page() {
 /**
  * Returns the Collection widget itself
  */
-function edshelf_collection_widget_embed( $id = EDSHELF_DEFAULT_COLLECTION_ID, $height = EDSHELF_DEFAULT_COLLECTION_HEIGHT ) {
-    $id = esc_attr( $id );
+function edshelf_collection_widget_embed( $id = EDSHELF_DEFAULT_COLLECTION_ID, $height = EDSHELF_DEFAULT_COLLECTION_HEIGHT, $type = EDSHELF_DEFAULT_COLLECTION_TYPE ) {
+    $id     = esc_attr( $id );
     $height = esc_attr( $height );
-    return "<div id='edshelf-widget'></div><script src='//edshelf.com/widgets/collection?id={$id}&height={$height}'></script>";
+    $type   = esc_attr( $type );
+    return "<div id='edshelf-widget'></div><script src='//edshelf.com/widgets/collection?id={$id}&height={$height}&type={$type}'></script>";
 }
 
 
 /**
  * Creates a template tag for the Collection widget
  *
- * edshelf_collection_widget( 8786, 500 );
+ * edshelf_collection_widget( 8786, 500, 'full' );
  */
-function edshelf_collection_widget( $id, $height ) {
-    echo edshelf_collection_widget_embed( $id, $height );
+function edshelf_collection_widget( $id, $height, $type ) {
+    echo edshelf_collection_widget_embed( $id, $height, $type );
 }
 
 
 /**
  * Creates a shortcode for the Collection widget
  *
- * [edshelf-collection-widget id="8786" height="500"]
+ * [edshelf-collection-widget id="8786" height="500" type="full"]
  */
 add_shortcode( 'edshelf-collection-widget', 'edshelf_collection_widget_function' );
 function edshelf_collection_widget_function( $atts ) {
     extract( shortcode_atts( array(
         'id'     => EDSHELF_DEFAULT_COLLECTION_ID,
-        'height' => EDSHELF_DEFAULT_COLLECTION_HEIGHT
+        'height' => EDSHELF_DEFAULT_COLLECTION_HEIGHT,
+        'type'   => EDSHELF_DEFAULT_COLLECTION_TYPE
     ), $atts ) );
 
     return edshelf_collection_widget_embed( $id );
@@ -109,12 +117,13 @@ function edshelf_collection_widget_wpwidget( $args ) {
     if( !is_array( $options ) ) {
         $options = array(
             'id'     => EDSHELF_DEFAULT_COLLECTION_ID,
-            'height' => EDSHELF_DEFAULT_COLLECTION_HEIGHT
+            'height' => EDSHELF_DEFAULT_COLLECTION_HEIGHT,
+            'type'   => EDSHELF_DEFAULT_COLLECTION_TYPE
         );
     }     
 
     echo $before_widget;
-    echo edshelf_collection_widget_embed( $options['id'], $options['height'] );
+    echo edshelf_collection_widget_embed( $options['id'], $options['height'], $options['type'] );
     echo $after_widget;
 }
 
@@ -129,13 +138,15 @@ function edshelf_collection_widget_control() {
     if( !is_array( $options ) ) {
         $options = array(
             'id'     => EDSHELF_DEFAULT_COLLECTION_ID,
-            'height' => EDSHELF_DEFAULT_COLLECTION_HEIGHT
+            'height' => EDSHELF_DEFAULT_COLLECTION_HEIGHT,
+            'type'   => EDSHELF_DEFAULT_COLLECTION_TYPE
         );
     }     
 
     if( $_POST['edshelf_collection_submit'] ) {
         $options['id']     = esc_attr( $_POST['edshelf_collection_id'] );
         $options['height'] = esc_attr( $_POST['edshelf_collection_height'] );
+        $options['type']   = esc_attr( $_POST['edshelf_collection_type'] );
         update_option( 'edshelf_collection_widget', $options );
     }
 
@@ -146,10 +157,11 @@ function edshelf_collection_widget_control() {
     <input type="text" id="edshelf-collection-id" name="edshelf_collection_id" size="5" maxlength="5" value="<?php echo $options['id'];?>">
     <label for="edshelf-collection-height">Widget height</label>:
     <input type="text" id="edshelf-collection-height" name="edshelf_collection_height" size="5" maxlength="6" value="<?php echo $options['height'];?>">
+    <label for="edshelf-collection-height">Widget type</label>:
+    <input type="text" id="edshelf-collection-type" name="edshelf_collection_type" size="5" maxlength="10" value="<?php echo $options['type'];?>">
     <input type="hidden" name="edshelf_collection_submit" value="1">
 </p>
 <p>The Collection ID can be found on the page for the collection on edshelf. Look for the code in the Widget module at the bottom of the right column. The ID will look like <code>id="8786"</code> in the code.</p>
-<p>NOTE: This is a full-page widget and will not fit into a sidebar.</p>
 <?php
 }
 
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Tags: edshelf, education, edtech, collection, apps, tools, embed, widget
 Contributors: edshelf, mikeleeorg
 Tested up to: 3.5
 Requires at least: 2.9
-Stable Tag: 0.1.0
+Stable Tag: 0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -15,11 +15,11 @@ Adds a shortcode and template tag for embedding an edshelf widget onto your site
 
 This plugin provides a shortcode so you can easily embed an edshelf widget onto your site.
 
-The shortcode is: `[edshelf-collection-widget id="NNNN" height="YYY"]`
+The shortcode is: `[edshelf-collection-widget id="NNNN" height="YYY" type="TTTT"]`
 
-The template tag is: `edshelf_collection_widget( NNNN, YYYY );`
+The template tag is: `edshelf_collection_widget( NNNN, YYYY, 'TTTT' );`
 
-Where `NNNN` is the ID of the collection you want to embed and `YYY` is the height of the widget in pixels. You can get this ID from the collection on edshelf, in the Widget module at the bottom of the right column.
+Where `NNNN` is the ID of the collection you want to embed, `YYY` is the height of the widget in pixels, and `TTTT` is the type of widget. You can get this ID from the collection on edshelf, in the Widget module at the bottom of the right column. The choices for the type of widget are "full" (the default setting), "compact", or "list".
 
 == Installation ==
 
@@ -30,8 +30,9 @@ Where `NNNN` is the ID of the collection you want to embed and `YYY` is the heig
 1. Look for the Widget module at the bottom of the right column. In that module will be some code.
 1. Look for a 4 or 5-digit number in that code. That number is the ID number of the collection.
 1. Go to the blog post or page on which you want to embed this collection widget.
-1. Type in the following WordPress shortcode in the text editor: `[edshelf-collection-widget id="NNNN" height="YYY"]`, where `NNNN` is the collection ID you just found and `YYY` is the height of the widget, in pixels.
-1. Or use the template tag `edshelf_collection_widget( $id, $height );` if you are familiar with PHP.
+1. Type in the following WordPress shortcode in the text editor: `[edshelf-collection-widget id="NNNN" height="YYY" type="TTTT"]`.
+1. The settings are: `NNNN` is the collection ID you just found. `YYY` is the height of the widget in pixels. `TTTT` is the type of widget, where your choices are "full" (the default setting), "compact", or "list".
+1. Or use the template tag `edshelf_collection_widget( $id, $height, '$type' );` if you are familiar with PHP.
 1. And you're done!
 
 == Frequently Asked Questions ==
@@ -52,6 +53,9 @@ As tall as you want!
 
 = How can I set the width of the widget? =
 We don't offer that option yet, but the widget will automatically stretch to 100% of whatever space you put it in - so if you know HTML and CSS, you can just set the width of the parent container holding this widget.
+
+= Can I embed more than one widget to a page? =
+Sorry, not yet. Our widget will not work properly if you try to embed more than one to a page. We will fix this in a later release.
 
 = Is this widget available in other languages? =
 Sorry, not yet. We do intend to offer multiple language support in the future though.
